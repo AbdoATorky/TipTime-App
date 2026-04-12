bd562cfce4726e8bb4adea81ae08bc68af317b13
AbdoATorky <abdalsalamtorky@gmail.com>
2026-04-12 05:42:58 +0200

    Implement tip calculation logic and enable ViewBinding

--- app/src/main/java/com/abs/tiptimeapp/MainActivity.kt
+++ app/src/main/java/com/abs/tiptimeapp/MainActivity.kt
@@ -5,18 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.abs.tiptimeapp.databinding.ActivityMainBinding
+import kotlin.math.ceil
+import kotlin.math.floor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        binding.calculateBtn.setOnClickListener {
+            val cost = binding.serviceEt.text.toString().toDouble()
+            val checkedRB = binding.group.checkedRadioButtonId
+            val tip = when(checkedRB){
+                R.id.amazing_rb ->0.2
+                R.id.good_rb -> 0.18
+                else -> 0.15
+            }
+            var total = cost * tip
+            if (binding.roundTipSwitch.isChecked)
+                total = floor(total)
+
+            binding.resultTv.text = "$$total"
+        }
 
     }
 }
